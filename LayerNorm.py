6a274bb43de3a4a1ba932a59a3b535e3def16dab
kienduynguyen <kienduynguyen94@gmail.com>
2018-04-17 11:52:43 +0900

    Fix forward path of LayerNorm. Use built-in gradcheck in Pytorch

--- LayerNorm.py
+++ LayerNorm.py
@@ -8,18 +8,18 @@ import torch.nn as nn
 
 from torch.nn.parameter import Parameter
 from torch.autograd import Function
+from torch.autograd import Variable, gradcheck
 from math import sqrt
 
 
 class layer_norm(Function):
 
 	@staticmethod
-	def forward(ctx, input, gain=None, bias=None, eps=1e-8):
+	def forward(ctx, input, gain=None, bias=None):
 		ctx.save_for_backward(input, gain, bias)
-		mean = input.mean()
-		var = input.var(unbiased=False)
-		input_normalized = (input - mean) / sqrt(var + eps)
-		ctx.constants = (mean, var, eps)
+		mean = input.mean(-1, keepdim=True)
+		var = input.var(-1, unbiased=False, keepdim=True)
+		input_normalized = (input - mean) / torch.sqrt(var + 1e-9)
 
 		if gain is not None and bias is not None:
 			output = input_normalized * gain + bias
@@ -33,21 +33,23 @@ class layer_norm(Function):
 	@staticmethod
 	def backward(ctx, grad_output):
 		input, gain, bias = ctx.saved_variables
-		mean, var, eps = ctx.constants
-		input_normalized = (input - mean) / sqrt(var + eps)
+		mean = input.mean(-1, keepdim=True)
+		var = input.var(-1, unbiased=False, keepdim=True)
+		input_normalized = (input - mean) / torch.sqrt(var + 1e-9)
 		grad_input = grad_gain = grad_bias = None
 
-		N = input.numel()
+		N = input.size(-1)
 		input_mu = input - mean
-		std_inv = 1. / sqrt(var + eps)
+		std_inv = 1. / torch.sqrt(var + 1e-9)
 
 		if ctx.needs_input_grad[0]:
 			if gain is not None:
 				grad_input_normalized = (grad_output * gain)
 			else:
 				grad_input_normalized = grad_output
-			grad_var = (-0.5) * (grad_input_normalized * input_mu).sum() * (std_inv ** 3)
-			grad_mean = (-1.0) * (grad_input_normalized * std_inv).sum() - 2.0 * grad_var * input_mu.mean()
+			grad_var = (-0.5) * (grad_input_normalized * input_mu).sum(dim=-1, keepdim=True) * (std_inv ** 3)
+			grad_mean = (-1.0) * (grad_input_normalized * std_inv).sum(dim=-1, keepdim=True) \
+				- 2.0 * grad_var * input_mu.mean(dim=-1, keepdim=True)
 			grad_input = grad_input_normalized * std_inv + (2. / N) * grad_var *  input_mu + (1. / N) * grad_mean
 		if gain is not None and ctx.needs_input_grad[1]:
 			grad_gain = (grad_output * input_normalized).sum(dim=0)
@@ -63,11 +65,9 @@ class LayerNorm(nn.Module):
 	Notes: This implement serves for the (N x C) tensor only where C is the number of features.
 
 	"""
-	def __init__(self, num_features, eps=1e-5, momentum=0.1, affine=True):
+	def __init__(self, num_features, affine=True):
 		super(LayerNorm, self).__init__()
 		self.affine = affine
-		self.eps = eps
-		self.momentum = momentum
 		if self.affine:
 			self.weight = Parameter(torch.Tensor(num_features))
 			self.bias = Parameter(torch.Tensor(num_features))
@@ -85,5 +85,15 @@ class LayerNorm(nn.Module):
 		return layer_norm.apply(input, self.weight, self.bias)
 
 	def __repr__(self):
-		return ("{name}(eps={eps}, momentum={momentum}, affine={affine})"
+		return ("{name}(num_features={num_features}, affine={affine})"
 			.format(name=self.__class__.__name__, **self.__dict__))
+
+
+if __name__ == "__main__":
+	torch.manual_seed(123)
+	input = (Variable(torch.randn(30, 20).double(), requires_grad=True),
+		Variable(torch.randn(20).double(), requires_grad=True),
+		Variable(torch.randn(20).double(), requires_grad=True))
+
+	test = gradcheck(layer_norm.apply, input)
+	print("test:", test)
